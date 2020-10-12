13630a31f72bd539a6b25e4f2d6e20f93977abec
cheapholidayticket <59072477+cheapholidayticket@users.noreply.github.com>
2020-10-12 11:22:27 +0800

    Updated icons, MainActivity UI with recycler view

--- app/src/main/java/com/wong/letsdiscusspsb/MainActivity.java
+++ app/src/main/java/com/wong/letsdiscusspsb/MainActivity.java
@@ -1,14 +1,35 @@
 package com.wong.letsdiscusspsb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Let's Discuss");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.naView);
+
+        View view 
+
     }
 }
